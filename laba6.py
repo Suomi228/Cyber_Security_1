01dcd3555816093917bb77c7b1ff8c9e3efa8427
Suomi228 <bixep345@gmail.com>
2023-11-12 16:51:27 +0300

    laba6

--- laba6.py
+++ laba6.py
@@ -1,165 +1,141 @@
+def parse_cell_coordinates(cells):
+    arr_cell = []
+    for cell in cells:
+        coordinates = cell.split(" ")
+        arr_cell.append([int(coordinates[0]) - 1, int(coordinates[1]) - 1])
+    return arr_cell
 
-def get_blocked_cells_list(col_n: int, row_n: int) -> list:
-    cell_num = input('Введите количество "запрещённых" ячеек: ')
-    while not (1 <= int(cell_num) < col_n * row_n):
-        print('Вы ввели не корректное число (число должно быть больше 0, но меньше кол-ва ячеек в таблице)')
-        cell_num = input('Введите количество "запрещённых" ячеек: ')
-    blocked_cells = []
-    for _ in range(int(cell_num)):
-        cell_col, cell_row = [*input('Введите координаты "запрещённой" ячейки (Формат: 1 1): ').split(), '', ''][:2]
-        while not (1 <= int(cell_col) <= col_n and
-                   1 <= int(cell_row) <= row_n):
-            if not (1 <= int(cell_col) <= col_n):
-                print('Вы ввели не корректное число столбцов (число должно быть больше 0, но меньше числа столбцов)')
-            elif not (1 <= int(cell_row) <= row_n):
-                print('Вы ввели не корректное число строк (число должно быть больше 0, но меньше числа столбцов)')
-            else:
-                print('Вы ввели не числа (число должно состоять только из цифр)')
-            cell_col, cell_row = [*input('Введите координаты "запрещённой" ячейки (Формат: 1 1): ').split(), '', ''][:2]
-        blocked_cells.append((int(cell_col) - 1, int(cell_row) - 1))
-    return blocked_cells
+def calculate_table_count(string, coll, line, cell_count):
+    len_str = len(string) + cell_count
+    cnt_table = len(string) // (coll * line - cell_count)
+    if len_str != coll * line:
+        cnt_table += 1
+    return cnt_table
 
-
-def code(key_index_to_real_index, col_n, row_n, encrypted_str, blocked_cells):
-    code_str = ''
-    # Кодируем всю строку по таблицам (запускаем цикл на необходимое кол-во таблиц)
-    while True:
-        code_table = [['' for _ in range(col_n)] for _ in range(row_n)]
-        for blocked_cell in blocked_cells:
-            code_table[blocked_cell[1]][blocked_cell[0]] = 'X'
-        n_counter = 0
-        m_counter = 0
-        # Проходимся по кодируемой строке (блок размером с таблицу или меньше) и заполняем табличку
-        while True:
-            # Иначе добавляем символ в нашу таблицу и удаляем его из текуущего куска шифра
-            if code_table[m_counter][n_counter] != '#':
-                code_table[m_counter][n_counter] = encrypted_str[0]
-                encrypted_str = encrypted_str[1:]
-                if not encrypted_str:
+def read_key(coll):
+    key = []
+    str_key = input("Введите ключ, отделяя каждую цифру пробелом: ").split(" ")
+    while len(str_key) != coll:
+        print("Количество цифр в ключе должно совпадать с количеством столбцов таблицы!")
+        str_key = input("Введите ключ, отделяя каждую цифру пробелом: ").split(" ")
+    for digit in str_key:
+        key.append(int(digit) - 1)
+    return key
+def encode_message(string, line, coll, arr_cell, cnt_table):
+    tables = [[[0 for _ in range(coll)] for _ in range(line)] for _ in range(cnt_table)]
+    k = 0
+    for i in range(len(tables)):
+        for j in range(line):
+            for l in range(coll):
+                if k >= len(string):
                     break
-
-            if n_counter == col_n - 1 and m_counter == row_n - 1:
+                elif is_cell_valid(arr_cell, j, l):
+                    tables[i][j][l] = string[k]
+                    k += 1
+                print(tables[i][j][l], end="\t")
+            if k >= len(string):
                 break
-
-            n_counter += 1
-            if n_counter == col_n:
-                m_counter += 1
-                n_counter = 0
-
-        # Выводим табличку и наш ключ сверху
-        print(' ', '    '.join(str(key_index + 1) for key_index in key_index_to_real_index.keys()))
-        print(*code_table, sep='\n')
+            print()
         print()
-        for blocked_cell in blocked_cells:
-            code_table[blocked_cell[1]][blocked_cell[0]] = ''
+    return tables
 
-        # Исходя из таблички заполняем строку шифра по столбцам
-        for col_index in sorted(key_index_to_real_index.keys()):
-            code_str += ''.join([row_list[key_index_to_real_index[col_index]] for row_list in code_table])
-        if not encrypted_str:
+def print_encoded_message(tables, coll, key):
+    print("\nЗашифрованное сообщение: ")
+    for table in tables:
+        for j in range(coll):
+            for l in range(len(tables[0])):
+                if table[l][index_of(key, j)] != 0:
+                    print(table[l][index_of(key, j)], end="")
+
+
+
+def decode_message(string, line, coll, arr_cell, cnt_table, key):
+    tables = [[[0 for _ in range(coll)] for _ in range(line)] for _ in range(cnt_table)]
+    remains = 0
+    k = 0
+    a = coll * line - len(arr_cell)
+
+    if len(string) < a:
+        remains = a - len(string)
+    else:
+        remains = a - len(string) % a
+
+    for i in range(line - 1, -1, -1):
+        for j in range(coll - 1, -1, -1):
+            if is_cell_valid(arr_cell, i, j):
+                tables[cnt_table - 1][i][j] = 1
+                remains -= 1
+            if remains == 0:
+                break
+        if remains == 0:
             break
 
-    return code_str
-
-
-def decode(key_index_to_real_index, col_n, row_n, encrypted_str, blocked_cells):
-    decode_str = ''
-    # Декодируем всю строку по таблицам (запускаем цикл на необходимое кол-во таблиц
-    while True:
-        decode_table = [['' for _ in range(col_n)] for _ in range(row_n)]
-        for blocked_cell in blocked_cells:
-            decode_table[blocked_cell[1]][blocked_cell[0]] = 'X'
-
-        if len(encrypted_str) < col_n * row_n - len(blocked_cells):
-            n_counter = 0
-            m_counter = 0
-            test_table = [['' for _ in range(col_n)] for _ in range(row_n)]
-            for blocked_cell in blocked_cells:
-                test_table[blocked_cell[1]][blocked_cell[0]] = 'X'
-            counter = len(encrypted_str)
-            while True:
-                if test_table[m_counter][n_counter] != 'X':
-                    test_table[m_counter][n_counter] = '_'
-                    counter -= 1
-                    if counter == 0:
+    for i in range(cnt_table):
+        for j in range(coll):
+            index = index_of(key, j)
+            for l in range(line):
+                if tables[i][l][index] != 1 and is_cell_valid(arr_cell, l, index):
+                    tables[i][l][index] = string[k]
+                    k += 1
+                    if k >= len(string):
                         break
-                n_counter += 1
-                if n_counter == col_n:
-                    m_counter += 1
-                    n_counter = 0
-            empty_place_in_cols = []
-            print(*test_table, sep='\n')
-            print(empty_place_in_cols, sep='\n')
-
-            for col_index in range(col_n):
-                empty_place_in_cols.append(len(['' for col_list in test_table if col_list[col_index] == '_']))
-
-        else:
-            empty_place_in_cols = [row_n for _ in range(col_n)]
-
-        n_counter = 0
-        m_counter = 0
-        # Проходимся по кодируемой строке (блок размером с таблицу или меньше) и заполняем табличку
-        while True:
-            # Иначе добавляем символ в нашу таблицу и удаляем его из текуущего куска шифра
-            if decode_table[m_counter][key_index_to_real_index[n_counter]] != 'X':
-                decode_table[m_counter][key_index_to_real_index[n_counter]] = encrypted_str[0]
-                encrypted_str = encrypted_str[1:]
-                if not encrypted_str:
-                    break
-
-            if n_counter == col_n - 1 and m_counter == row_n - 1:
+            if k >= len(string):
                 break
 
-            m_counter += 1
-            if m_counter == row_n or m_counter + 1 > empty_place_in_cols[key_index_to_real_index[n_counter]]:
-                n_counter += 1
-                m_counter = 0
+    return tables
 
-        # Выводим табличку и наш ключ сверху
-        print(' ', '    '.join(str(key_index + 1) for key_index in key_index_to_real_index.keys()))
-        print(*decode_table, sep='\n')
+
+def print_decoded_message(tables):
+    found_flag = False
+    for table in tables:
+        for row in table:
+            for cell in row:
+                if cell == 1:
+                    found_flag = True
+                    break
+                print(cell, end="\t")
+            if found_flag:
+                break
+            print()
+        if found_flag:
+            break
         print()
 
-        for blocked_cell in blocked_cells:
-            decode_table[blocked_cell[1]][blocked_cell[0]] = ''
+    print("\nРасшифрованное сообщение: ")
+    for table in tables:
+        for row in table:
+            for cell in row:
+                if cell != 1 and cell != 0:
+                    print(cell, end="")
+def is_cell_valid(cells, i, j):
+    for cell in cells:
+        if cell[0] == i and cell[1] == j:
+            return False
+    return True
 
-        for row_list in decode_table:
-            for col in row_list:
-                decode_str += col
-        if not encrypted_str:
-            break
-
-    return decode_str
+def index_of(key, index):
+    for i in range(len(key)):
+        if key[i] == index:
+            return i
+    return -1
 
 
-print('Программа реализует решение лабораторной работы №6: "Перестановка, усложненная по таблице"')
-intent = int(input("ВВЕДИТЕ 1 ЕСЛИ ХОТИТЕ ЗАШИФРОВАТЬ, 2 ЕСЛИ РАСШИФРОВАТЬ:"))
-while intent not in range(1, 2 + 1):
-    intent = int(input("ВВЕДИТЕ 1 ЕСЛИ ХОТИТЕ ЗАШИФРОВАТЬ, 2 ЕСЛИ РАСШИФРОВАТЬ:"))
+print("Введите 1 если хотите зашифровать, 2 если расшифровать")
 
-message = input("ВВЕДИТЕ СООБЩЕНИЕ:")
-print("Длина сообщения:", len(message))
-col_num = int(input('Введите количество столбцов: '))
-while col_num < 0:
-    col_num = input('Введите количество столбцов: ')
+choice = int(input())
+str_input = input("Введите сообщение: ")
+line_input = int(input("Введите количество строк таблицы: "))
+coll_input = int(input("Введите количество столбцов таблицы: "))
+cells = input("Введите координаты ячеек через запятую(Формат ввода: 1 1, 2 4, 3 3) где первое число это номер строки, а второе - номер столбца: ").split(", ")
+arr_cell = parse_cell_coordinates(cells)
+key = read_key(coll_input)
+cnt_table = calculate_table_count(str_input, coll_input, line_input, len(arr_cell))
 
-row_num = int(input('Введите количество строк: '))
-while row_num < 0:
-    row_num = int(input('Введите количество строк: '))
-key_str = input("ВВЕДИТЕ КЛЮЧ (Формат ввода: 1-1-1-1): ")
-while len(key_str.split('-'))!=col_num:
-    key_str = input("ВВЕДИТЕ КЛЮЧ (Формат ввода: 1-1-1-1): ")
-
-key_list = [int(key_symbol) for key_symbol in key_str.split('-')]
-key = {}
-real_index = 0
-for index in key_list:
-    key[index - 1] = real_index
-    real_index += 1
-blocked_cells_list = get_blocked_cells_list(col_num, row_num)
-
-if intent==1:
-    print('Шифр:', code(key, col_num, row_num, message, blocked_cells_list))
+if choice==1:
+    tables = encode_message(str_input, line_input, coll_input, arr_cell, cnt_table)
+    print_encoded_message(tables, coll_input, key)
+elif choice==2:
+    tables = decode_message(str_input, line_input, coll_input, arr_cell, cnt_table, key)
+    print_decoded_message(tables)
 else:
-    print('Дешифровка:', decode(key, col_num, row_num, message, blocked_cells_list))
+    print("Ошибка ввода!")
